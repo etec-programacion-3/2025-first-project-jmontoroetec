de0f481318d6f61c97a29eb44c636adeb43675a3
jmontoroetec <j.montoro@alumno.etec.um.edu.ar>
2025-04-09 12:31:45 -0300

    jaja

--- aa.py
+++ aa.py
@@ -2,6 +2,6 @@ def resta():
     numero1=1
     numero2=2
     resultado=numero1-numero2
-    print("El resultado de la resta es: ", resultado)
+    print("El resultado de la resta: ", resultado)
     return resultado
 resta()
